82a3a0309b136fed9cc6d09a10941a0355f3b9d2
sunilkalluru1332 <sunil.kalluru@riversideinsights.com>
2024-09-28 10:14:37 +0530

    first commit

---
+++ src/test/java/com/petstore/api/tests/PetStoreTests.java
@@ -0,0 +1,33 @@
+package com.petstore.api.tests;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class PetStoreTests {
+
+    @BeforeClass
+    public void setup() {
+        // Base URI for PetStore API
+        RestAssured.baseURI = "https://petstore.swagger.io/v2";
+    }
+
+    @Test
+    public void testFindPetsByStatus() {
+        // Sending a GET request to find pets by status
+        Response response = RestAssured.given()
+                .queryParam("status", "available")
+                .when()
+                .get("/pet/findByStatus")
+                .then()
+                .contentType(ContentType.JSON)
+                .extract()
+                .response();
+
+        // Assertion to verify that the status code is 200
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code is not 200!");
+    }
+}
